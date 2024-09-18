7f96d2c49106fae9f081d9e0dc880a28cd275242
Andrew Leech <andrew.leech@planetinnovation.com.au>
2024-09-18 14:49:51 +1000

    Detect / specify micropython directory.
    Allows running from anywhere in micropython tree.
    Alternatively specify micropython root directory with
    environment variable: MICROPY_DIR

--- src/mpbuild/__init__.py
+++ src/mpbuild/__init__.py
@@ -4,13 +4,20 @@ __version__ = "0.1.0"
 from enum import Enum
 from .find_boards import ports_and_boards, board_db
 
-# For testing only!
-import os
 
-try:
-    os.stat("./ports")
-except OSError:
-    raise SystemExit("Please run from root of micropython source tree")
+def find_micropython_root():
+    root = Path(os.environ.get("MICROPY_DIR", ".")).resolve()
+    while True:
+        if (root / "ports").exists() and (root / "mpy-cross").exists():
+            return root, port
+        
+        if root.parent == root:
+            raise SystemExit("Please run from micropython source tree or specify with env: MICROPY_DIR")
+        root = root.parent
+        
+
+root, port = find_micropython_root()
+os.chdir(root)
 
 
 ports = list(ports_and_boards().keys())
