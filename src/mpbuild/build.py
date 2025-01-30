a26c21916c6afddea6ccbc31149fe06452e8d10f
Hans Märki <buhtig.hans.maerki@ergoinfo.ch>
2025-01-30 13:39:41 +0100

    remove BOARD parameter from 'make submodules' (#61)

--- src/mpbuild/build.py
+++ src/mpbuild/build.py
@@ -98,9 +98,7 @@ def docker_build_cmd(
     args = " " + " ".join(extra_args)
 
     make_mpy_cross_cmd = "make -C mpy-cross && "
-    update_submodules_cmd = (
-        f"make -C ports/{port.name} submodules BOARD={board.name}{variant_cmd} && "
-    )
+    update_submodules_cmd = f"make -C ports/{port.name} submodules && "
 
     uid, gid = os.getuid(), os.getgid()
 
